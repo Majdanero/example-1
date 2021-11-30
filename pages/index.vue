2da62dec71b8ebf14c04e97f13937ea98e7ee705
Szymon Majdanik <szymon.majdanik@gmail.com>
2021-11-30 20:27:38 +0100

    123

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+section
+	h1 Home 1231123123
+	p {{message}}
+	p {{result}}
+	input(v-model="message" )
+</template>
+<script setup lang="ts">
+	const message = ref('Hello');
+	const result = computed(() => {
+		return message.value + ' computed'
+	})
+</script>
+<style lang="scss">
+	section {
+		h1 {
+			color: red;
+		}
+	}
+</style>
